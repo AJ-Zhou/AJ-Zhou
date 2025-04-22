64776bfa0c78dca96b6b300fe845d710ba94c90e
lingsongfeng <36920701+lingsongfeng@users.noreply.github.com>
2025-04-22 16:30:18 +0000

    Deploying to gh-pages from @ AJ-Zhou/AJ-Zhou.github.io@d14e196861c3320a98111895288f3e3a4c6ee132 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,30 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-four-paper-accepted-to-iclr2025",
+            },},{id: "news-four-paper-accepted-to-iclr2025",
           title: 'Four paper accepted to ICLR2025',
           description: "",
           section: "News",},{
